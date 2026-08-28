0159b95d3e0eb685f679e4281736144f59982caf
Rohan S Bhat <76154331+R451748@users.noreply.github.com>
2026-08-28 16:35:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -59,7 +59,7 @@ def ask(query: str):
         """
 
         response = client.chat.completions.create(
-           model="llama-3.3-70b-versatile",
+           model="llama-3.1-8b-instant",
             messages=[{"role": "user", "content": prompt}]
         )
 
